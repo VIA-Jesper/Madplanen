b5ae772b1bbbeadeee79d3a3a64ff475dd32b929
Stygg <48440436+VIA-Jesper@users.noreply.github.com>
2020-09-29 18:27:08 +0200

    Initial

---
+++ app/src/main/java/dk/ipfortify/madplanen/MainActivity.java
@@ -0,0 +1,13 @@
+package dk.ipfortify.madplanen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
